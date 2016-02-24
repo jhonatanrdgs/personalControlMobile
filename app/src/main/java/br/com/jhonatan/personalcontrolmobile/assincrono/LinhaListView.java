4ab7dbfc084a431473193d0e5eb962ce8c46d054
Jhonatan Rodrigues <jhonatan.rodrigues@ST.dom>
2016-02-24 16:28:52 -0300

    LinhaListView

---
+++ app/src/main/java/br/com/jhonatan/personalcontrolmobile/assincrono/LinhaListView.java
@@ -0,0 +1,10 @@
+package br.com.jhonatan.personalcontrolmobile.assincrono;
+
+/**
+ * Created by jhonatan.rodrigues on 24/02/2016.
+ */
+public interface LinhaListView {
+
+    String getPrimeiraLinha();
+    String getSegundaLinha();
+}
